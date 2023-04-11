9b4fba334356784ac7879c980f7eb40835a42e82
danielbarretoes <125984531+danielbarretoes@users.noreply.github.com>
2023-04-11 17:29:35 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import QR from "./components/QR";
+
+const App = () => {
+  return <QR qrValue={"uniq.com/sharedview/64615-65461-165161-1651"}/>;
+};
+
+export default App;
